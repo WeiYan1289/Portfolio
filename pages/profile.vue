aef70c88967e3b0e3baf66a348bb98e43c9cb602
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-02 22:24:11 +0800

    feat: Better layout

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,13 +1,48 @@
 <template>
   <div class="profile">
-    <img class="my-photo" src="~/assets/images/profile.jpg" alt="my avatar" />
-    <div class="intro">
-      <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eveniet
-        quod nostrum dolore ipsam blanditiis repellendus cum maiores facilis,
-        laudantium accusantium quasi perferendis. Facilis minima eligendi
-        cupiditate ipsum similique commodi!
-      </span>
+    <div class="profile__container">
+      <div class="profile__container__left">
+        <img
+          class="my-photo"
+          src="~/assets/images/profile.jpg"
+          alt="my avatar"
+        />
+      </div>
+      <div class="profile__container__right">
+        <div class="the-title">Hello, my name Wei Yan!</div>
+        <div class="para">
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
+            facilis, laudantium accusantium quasi perferendis. Facilis minima
+            eligendi cupiditate ipsum similique commodi!
+          </span>
+        </div>
+        <div class="para">
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
+            facilis, laudantium accusantium quasi perferendis. Facilis minima
+            eligendi cupiditate ipsum similique commodi!
+          </span>
+        </div>
+        <div class="para">
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
+            facilis, laudantium accusantium quasi perferendis. Facilis minima
+            eligendi cupiditate ipsum similique commodi!
+          </span>
+        </div>
+        <div class="para">
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
+            facilis, laudantium accusantium quasi perferendis. Facilis minima
+            eligendi cupiditate ipsum similique commodi!
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,22 +50,43 @@
 <script>
 export default {
   head: () => ({
-    title: 'My Profile',
-  }),
+    title: "My Profile"
+  })
 };
 </script>
 
 <style lang="scss" scoped>
 .profile {
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 200px;
+
+  &__container {
     display: flex;
-    flex-direction: row;
-    margin-left: 200px;
+    width: 50vw;
+
+    &__left {
+      padding: 30px;
+    }
+
+    &__right {
+      padding-top: 30px;
+      margin-left: 50px;
+      .the-title {
+        font-size: 2em;
+        font-weight: 800;
+      }
+      .para {
+        margin-top: 40px;
+        font-size: 1.75em;
+      }
+    }
+  }
 }
 
 .my-photo {
   max-width: 250px;
   max-height: 250px;
-  border-radius: 40%;
 }
 
 .intro {
