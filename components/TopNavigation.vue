350513d879e58d1b63a37f6deb1c408923ddb20a
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-02 22:07:10 +0800

    style: Fixed stylings and indentation

--- components/TopNavigation.vue
+++ components/TopNavigation.vue
@@ -1,73 +1,66 @@
 <template>
-    <div class="top-nav">
-        <div class="logo">
-            <nuxt-link class="nuxt" to="/">
-            <img
-                class="logo-img"
-                src="~/assets/images/logo.svg"
-                alt="WY Logo"
-            />
-            </nuxt-link>
-        </div>
-        <div class="wrapper">
-            <div class="links">
-            <nuxt-link class="nuxt" to="/profile">
-                <div class="link" :class="{ 'link-active': isProfile }">
-                    about me
-                    <v-icon v-if="isProfile">mdi-folder-account</v-icon>
-                    <v-icon v-if="isProfile">mdi-card-account-details</v-icon>
-                </div>
-            </nuxt-link>
-            <nuxt-link class="nuxt" to="/education">
-                <div class="link" :class="{ 'link-active': isEducation }">
-                    educations
-                    <v-icon v-if="isEducation">mdi-school</v-icon>
-                    <v-icon v-if="isEducation">mdi-book-open-page-variant</v-icon>
-                </div>
-            </nuxt-link>
-            <nuxt-link class="nuxt" to="/projects">
-                <div class="link" :class="{ 'link-active': isProjects }">
-                    projects
-                    <v-icon v-if="isProjects">mdi-puzzle</v-icon>
-                    <v-icon v-if="isProjects">mdi-clipboard-text-search</v-icon>
-                </div>
-            </nuxt-link>
-            </div>
-        </div>
+  <div class="top-nav">
+    <div class="logo">
+      <nuxt-link class="nuxt" to="/">
+        <img class="logo-img" src="~/assets/images/logo.svg" alt="WY Logo" />
+      </nuxt-link>
     </div>
+    <div class="wrapper">
+      <div class="links">
+        <nuxt-link class="nuxt" to="/profile">
+          <div class="link" :class="{ 'link-active': isProfile }">
+            about me
+            <v-icon v-if="isProfile">mdi-folder-account</v-icon>
+            <v-icon v-if="isProfile">mdi-card-account-details</v-icon>
+          </div>
+        </nuxt-link>
+        <nuxt-link class="nuxt" to="/education">
+          <div class="link" :class="{ 'link-active': isEducation }">
+            educations
+            <v-icon v-if="isEducation">mdi-school</v-icon>
+            <v-icon v-if="isEducation">mdi-book-open-page-variant</v-icon>
+          </div>
+        </nuxt-link>
+        <nuxt-link class="nuxt" to="/projects">
+          <div class="link" :class="{ 'link-active': isProjects }">
+            projects
+            <v-icon v-if="isProjects">mdi-puzzle</v-icon>
+            <v-icon v-if="isProjects">mdi-clipboard-text-search</v-icon>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   computed: {
     isProfile() {
-      if(this.$route.path == "/profile"){
+      if (this.$route.path == "/profile") {
         return true;
-      }
-      else{
+      } else {
         return false;
       }
     },
 
     isEducation() {
-    if(this.$route.path == "/education"){
+      if (this.$route.path == "/education") {
         return true;
-      }
-      else{
+      } else {
         return false;
       }
     },
-    
+
     isProjects() {
-    if(this.$route.path == "/projects"){
+      if (this.$route.path == "/projects") {
         return true;
-      }
-      else{
+      } else {
         return false;
       }
-    },
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -91,7 +84,7 @@ export default {
   text-decoration: none;
 }
 .link {
-  color: #A8A8A8!important;
+  color: #a8a8a8 !important;
   width: 300px;
 }
 .link-active {
