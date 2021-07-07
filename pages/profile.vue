5258def118c96c189ff7822c2ba30e6f5e22fdd9
WeiYan <weiyan1289@gmail.com>
2021-07-07 12:13:54 +0800

    fix: completed filling all data

--- pages/profile.vue
+++ pages/profile.vue
@@ -9,38 +9,48 @@
         />
       </div>
       <div class="profile-container-right">
-        <div class="the-title">Hello, my name Wei Yan!</div>
+        <div class="the-title">Some Information About Me...... </div>
         <div class="para">
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
-            facilis, laudantium accusantium quasi perferendis. Facilis minima
-            eligendi cupiditate ipsum similique commodi!
-          </span>
+          <p>
+            Hi, my name is Wei Yan. I'm a I am <span>Fresh Graduate Software Engineer</span> who 
+            seeking a full-time position in the field of IT industry, where I can 
+            apply my knowledges and skills for continuous improvement.
+          </p>
         </div>
         <div class="para">
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
-            facilis, laudantium accusantium quasi perferendis. Facilis minima
-            eligendi cupiditate ipsum similique commodi!
-          </span>
+          <p>
+            For some demographics information about myself, I was born in year <span>1999</span>
+            and I come from <span>Tangkak Johor</span>, a place of Johor which nearby Melaka state. 
+            It is a small place but full with delicious foods and beautiful scenery.
+          </p>
         </div>
         <div class="para">
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
-            facilis, laudantium accusantium quasi perferendis. Facilis minima
-            eligendi cupiditate ipsum similique commodi!
-          </span>
+          <p>
+            Before I start my degree in UTAR, I rarely expose to programming and coding
+            related stuffs. After getting deeper and deeper about the programming, I found
+            that coding are actually <span>interesting</span> as it can achieve almost all the things 
+            I want to achieve and the results I want the system to behave. Therefore, <span>I'm
+            glad</span> that I chose <span>Software Engineering</span> as my Degree course and I decide to keep
+            improving my skills and knowledge to make me more familiar and comfortable while
+            coding.  
+          </p>
         </div>
         <div class="para">
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            eveniet quod nostrum dolore ipsam blanditiis repellendus cum maiores
-            facilis, laudantium accusantium quasi perferendis. Facilis minima
-            eligendi cupiditate ipsum similique commodi!
-          </span>
+          <p>
+            Although I feel that programming is interesting but sometimes <span>problems</span> and <span>challenges</span>
+            will arise while I am dealing with it. While I face problems and challenges while coding, 
+            I will google and search for any possible way to overcome it. Besides that, I will watch 
+            some videos regarding technologies to strengthen my fundamental and improving 
+            my knowledge so I can get more ideas about some projects I would like to start in 
+            the future.
+          </p>
+        </div>
+        <div class="para">
+          <p>
+            Other than technology, I like to play <span>badminton</span> and <span>snooker</span> during my free time. I 
+            started to play badminton while I was in my primary school. For snooker, I started to 
+            expose it while I come to university.
+          </p>
         </div>
       </div>
     </div>
@@ -72,12 +82,19 @@ export default {
       padding-top: 30px;
 
       .the-title {
-        font-size: 2em;
+        font-size: 1.7em;
         font-weight: 800;
       }
       .para {
         margin-top: 40px;
-        font-size: 1.75em;
+        font-size: 1.45em;
+
+        > p {
+          > span {
+            font-weight: bold;
+            color: brown;
+          }
+        }
       }
     }
   }
@@ -114,12 +131,12 @@ export default {
         padding-top: 0;
 
         .the-title {
-          font-size: 1.5em;
+          font-size: 1.4em;
           font-weight: 700;
         }
         .para {
           margin-top: 20px;
-          font-size: 1.3em;
+          font-size: 1.25em;
         }
       }
     }
